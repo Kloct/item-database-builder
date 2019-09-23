eb06b129d45f1c8f1e6dbf08b789bc6f5845cb3b
Kloct <Upfrontsuperior@gmail.com>
2019-09-22 21:41:38 -0400

    Column combining and formatting

--- builder4.js
+++ builder4.js
@@ -20,70 +20,48 @@ function populateData(dir) {
 //parse headers for db columns
 
 function parseColumnName(data){
-  let columns = []
-  for (i=0;i<data.length;i++){
-    columns = unique(columns, Object.keys(data[i]))
+  //Combine all properties into one array
+  let headersUnformatted = Object.entries(Object.assign({}, ...data)),
+    headersFormatted = []
+
+  // Format array for database
+  for(i=0;i<headersUnformatted.length;i++){
+    if (typeof headersUnformatted[i][1]==="number") headersUnformatted[i][1]="int(11)"
+    else if (typeof headersUnformatted[i][1]==="string") headersUnformatted[i][1]="varchar(50)"
+    else if (typeof headersUnformatted[i][1]==="boolean") headersUnformatted[i][1]="bit"
+    else console.error(`Unhandled column type: ${typeof headersUnformatted[i][1]} in ${headersUnformatted[i][0]}`)
+    headersFormatted.push(`${headersUnformatted[i][0]} ${headersUnformatted[i][1]}`)
   }
-  return columns
+return headersFormatted
 }
 
-function parseColumnType(merged, headers){
-  let types = [],
-  //go though all headers
-  for(h=0;h<headers.length;h++){
-    //go though all items
-    for(i=0;i<merged.length;i++){
-      //does property exist?
-      if(merged[i][headers[h]]){
-        types.push(typeof merged[i][headers[h]])
-        break;
-      }
-    }
-  }
-  return types
-}
+function createTableStatements(data){
 
-function unique (a, b) {
-  a = a.concat(b.filter(item => {
-    return a.indexOf(item) < 0;
-  }))
-  return a
 }
-
-function parseData() {
-  return true
-}
-
 
 /*//////////////
 // Run Things //
 //////////////*/
 async function buildData(){
   console.log(`====================`)
-  let mergedStrings = await populateData(stringsDir),
-  mergedData = await populateData(dataDir)
+  let mergedStrings = populateData(stringsDir),
+  mergedData = populateData(dataDir)
   console.log('Merged files')
   console.log(`mergedStrings: ${mergedStrings.length}`)
   console.log(`mergedData: ${mergedData.length}`)
 
   console.log(`====================`)
-  let headersStrings = await parseColumnName(mergedStrings),
-  headersData = await parseColumnName(mergedData)
+  let headersStrings = parseColumnName(mergedStrings),
+  headersData = parseColumnName(mergedData)
   console.log('Got Headers')
-  console.log(`headersStrings: ${headersStrings}`)
-  console.log(`headersData: ${headersData}`)
+  console.log(`headersStrings: ${headersStrings.length}`)
+  console.log(`headersData: ${headersData.length}`)
 
   console.log(`====================`)
-  let headersStringsType = await parseColumnType(mergedStrings, headersStrings),
-  headersDataType = await parseColumnType(mergedData, headersData)
-  console.log('Got Header Type')
-  console.log(`headersStringsType: ${headersStringsType}`)
-  console.log(`headersDataType: ${headersDataType}`)
+  console.log(`Would start to initialize database schema here`)
 
   console.log(`====================`)
-  let parsedStrings = await parseData(mergedStrings),
-  parsedData = await parseData(mergedData)
-  console.log('Parsed Data')
+  console.log(`Would populate newly created tables here`)
 
   process.exit(0);
 }
